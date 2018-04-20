63c44df5e6c6be0456be8f3ab3e4e110f2a8936e
oanaunc <34318423+oanaunc@users.noreply.github.com>
2018-04-20 19:47:27 +0300

    Add files via upload

---
+++ ConsignmentShop.cs
@@ -0,0 +1,119 @@
+﻿using ConsignmentShopLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsignmentShopUI
+{
+    public partial class ConsignmentShop : Form
+    {
+        private Store store = new Store();
+        BindingSource itemsBinding = new BindingSource();
+
+        private List<Item> shoppingCartData = new List<Item>();
+        BindingSource cartBinding = new BindingSource();
+
+        BindingSource vendorsBinding = new BindingSource();
+
+        private decimal storeProfit = 0;
+           
+
+        public ConsignmentShop()
+        {
+            InitializeComponent();
+            SetupData();
+
+            // legatura intre
+            itemsBinding.DataSource = store.Items.Where(x => x.Sold == false).ToList();
+            itemsListbox.DataSource = itemsBinding;
+            itemsListbox.DisplayMember = "Display";
+            itemsListbox.ValueMember = "Display";
+
+            cartBinding.DataSource = shoppingCartData;
+            shoppingCartListbox.DataSource = cartBinding;
+            shoppingCartListbox.DisplayMember = "Display";
+            shoppingCartListbox.ValueMember = "Display";
+
+            vendorsBinding.DataSource = store.Vendors;
+            vendorListBox.DataSource = vendorsBinding;
+            vendorListBox.DisplayMember = "Display";
+            vendorListBox.ValueMember = "Display";
+        }
+
+        private void SetupData()
+        {
+            store.Vendors.Add(new Vendor { FirstName = "Bill", LastName = "Smith" });
+            store.Vendors.Add(new Vendor { FirstName = "Sue", LastName = "Jones" });
+
+            store.Items.Add(new Item
+            {
+                Title = "Moby Dick",
+                Description = "A book about a whale",
+                Owner = store.Vendors[0],
+                Price = 4.50M
+            });
+
+
+            store.Items.Add(new Item
+            {
+                Title = "A tale of Two Cities",
+                Description = "A book about a revolution",
+                Owner = store.Vendors[1],
+                Price = 3.80M
+            });
+
+
+            store.Items.Add(new Item
+            {
+                Title = "Harry Potter Book 1",
+                Description = "A book about a boy",
+                Owner = store.Vendors[1],
+                Price = 5.20M
+            });
+
+
+            store.Items.Add(new Item
+            {
+                Title = "Jane Eyre",
+                Description = "A book about a girl",
+                Owner = store.Vendors[0],
+                Price = 1.50M
+            });
+
+            store.Name = "Seconds are Better";
+
+        }
+
+        private void addToCart_Click(object sender, EventArgs e)
+        {
+            Item selectedItem = (Item)itemsListbox.SelectedItem;
+
+            shoppingCartData.Add(selectedItem);
+            cartBinding.ResetBindings(false);
+        }
+
+        private void makePurchase_Click(object sender, EventArgs e)
+        {
+            foreach (Item item in shoppingCartData)
+            {
+                item.Sold = true;
+                item.Owner.PaymentDue += (decimal)item.Owner.Commision * item.Price;
+                storeProfit += (1 - (decimal)item.Owner.Commision) * item.Price;
+            }
+
+            shoppingCartData.Clear();
+            itemsBinding.DataSource = store.Items.Where(x => x.Sold == false).ToList();
+            storeProfitValue.Text = string.Format("$ {0}", storeProfit);
+
+            cartBinding.ResetBindings(false);
+            itemsBinding.ResetBindings(false);
+            vendorsBinding.ResetBindings(false);
+        }
+    }
+}
